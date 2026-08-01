bbe3ddf7e594d596622389bb0162f1b20585f1b5
Khushi Gupta <kg9610080@gmail.com>
2026-07-31 23:47:55 -1200

    Update app.py

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ class SkillTracker:
 st.title("My Skills Progress Tracker")
 st.write("Tracking my journey in Python, SQL, and Power BI")
 password= st.text_input("ENTER PASSWORD" , type='paasword')
-edit_mode = (pass=="200630")
+edit_mode = (password =="200630")
 
 tracker = SkillTracker()
 
